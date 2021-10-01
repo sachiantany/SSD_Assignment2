043e0abf718b3c7cc5476bc3720cb8eda0371692
sachiantany <sachiantany>
2021-10-02 00:10:16 +0530

    UI changes and bug fix

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -18,11 +18,11 @@ const Home = () => {
     <Grow in>
       <Container>
         <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+        <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
           <Grid item xs={12} sm={7}>
             <Posts setCurrentId={setCurrentId} />
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Form currentId={currentId} setCurrentId={setCurrentId} />
           </Grid>
         </Grid>
       </Container>
